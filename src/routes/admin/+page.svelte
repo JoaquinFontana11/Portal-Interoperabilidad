33020b4d74c41e6a3bad5022597528603b864242
Camilo Di Paolo <camilodipaolo10@gmail.com>
2022-11-16 11:54:23 -0300

    Agregada un parrafo al panel de admin jeje

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,1 +1,5 @@
-<p>admin</p>
+<main class="ml-56 dark:bg-gray-900 bg-gray-100 h-screen relative overflow-y-scroll">
+	<div class="w-3/4 h-3/4 absolute bottom-1/2 right-1/2 transform translate-x-1/2 translate-y-1/2">
+		<p class="dark:text-white">Bienvenido al panel de administrador</p>
+	</div>
+</main>
