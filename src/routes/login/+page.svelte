591ad66c0f8d0ba2b9ca62a12efd4f98f4dfe5ae
Camilo Di Paolo <camilodipaolo10@gmail.com>
2022-11-16 11:46:31 -0300

    Agregado estilo al formulario de login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,8 +1,47 @@
 <script lang="ts">
 </script>
 
-<form method="POST" action="?/login">
+<!-- <form method="POST" action="?/login">
 	<input type="text" placeholder="username" name="username" />
 	<input type="password" placeholder="password" name="password" />
 	<input type="submit" />
-</form>
+</form> -->
+<main class="min-h-screen bg-section z-10">
+	<form
+		method="POST"
+		action="?/login"
+		class="absolute top-1/2 left-1/2 h-74 bg-white shadow-lg rounded-lg transform -translate-x-1/2 -translate-y-1/2 p-10"
+	>
+		<div class="mb-6">
+			<label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+				>Nombre de usuario</label
+			>
+			<input
+				type="username"
+				id="username"
+				name="username"
+				class="bg-gray-50 outline-none border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"
+				placeholder="name@flowbite.com"
+				required
+			/>
+		</div>
+		<div class="mb-6">
+			<label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+				>Contraseña</label
+			>
+			<input
+				type="password"
+				id="password"
+				name="password"
+				class="bg-gray-50 border outline-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-sky-500 focus:border-sky-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"
+				required
+			/>
+		</div>
+
+		<button
+			type="submit"
+			class="text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm w-full  px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
+			>Ingresar</button
+		>
+	</form>
+</main>
