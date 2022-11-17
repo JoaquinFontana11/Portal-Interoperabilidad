3735c4593bcef0f9a9901f06314c527bc0e67009
Joaquin <joaquinfontana11@gmail.com>
2022-11-17 08:53:45 -0300

    Modificado estilo de novedad

--- src/routes/pages/catalogo/novedades/[slug]/+page.svelte
+++ src/routes/pages/catalogo/novedades/[slug]/+page.svelte
@@ -4,15 +4,20 @@
 	let novelty = JSON.parse(data.novelty);
 </script>
 
-<div class="p-10">
-	<img class="rounded-t-lg p-5 float-right w-1/2 h-1/2" src={novelty.image} alt="" />
-	<h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
-		{novelty.title}
-	</h5>
-	<p class="text-2xl font-bold tracking-tight text-gray-600 dark:text-white mb-2">
-		{novelty.summary}
-	</p>
-	<div class="text-justify indent-5">
-		{@html novelty.body}
+<div class="p-10 ">
+	<div class="bg-gray-50 rounded-md shadow-md p-10">
+		<img class="rounded-t-lg p-5 float-right w-1/2 h-1/2" src={novelty.image} alt="" />
+		<p class="text-gray-500 text-sm">{novelty.date.split('T')[0]}</p>
+		<h2
+			class="mb-2 tracking-tight text-sky-400 text-4xl font-extrabold leading-none md:text-5xl lg:text-6xl"
+		>
+			{novelty.title}
+		</h2>
+		<p class=" tracking-tight font-semibold text-gray-500 text-xl mb-2 mt-2">
+			{novelty.summary}
+		</p>
+		<div class="text-justify indent-5 mb-3 text-lg font-light text-gray-500 md:text-xl">
+			{@html novelty.body}
+		</div>
 	</div>
 </div>
